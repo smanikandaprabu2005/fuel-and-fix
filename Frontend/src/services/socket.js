72f053f1f78625999f18831cafa1675d33a6e864
Manikanda Prabu <smanikandaprabu2005@gmail.com>
2025-11-07 20:31:29 +0530

    update

--- Frontend/src/services/socket.js
+++ Frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.VITE_API_URL || "https://fuel-and-fix.onrender.com";
+const SOCKET_URL = import.meta.env.VITE_API_URL;
 
 //|| 'http://localhost:5000';
 
